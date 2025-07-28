391b9a5ad632842975ce8e91be893cd32ea2ba83
Saleh <103447502+salehdemir@users.noreply.github.com>
2025-07-29 01:00:12 +0430

    add more questions

--- questions.js
+++ questions.js
@@ -20,12 +20,48 @@ const  questions = [
                     "<href>","<hyperlink>"],
                     correctAnwer: 1,
             },
+                 {
+                question:"What does HTML stand for?",
+                options:["Hyper Trainer Marking Language"," Hyper Text Markup Language",
+                    "Hyper Text Marketing Language","Hyperlink and Text Markup Language"],
+                    correctAnwer: 1,
+            },
+            {
+                question:"How do you insert an image in an HTML page?",
+                options:['<img href="image.jpg">','<img src="image.jpg" alt="Description">',
+                    '<img src="image.jpg" alt="Description">','<picture src="image.jpg">'],
+                    correctAnwer: 1,
+            },
+            {
+                question:"Which tag is used to create a hyperlink in HTML?",
+                options:["<links>"," <a>",
+                    "<href>","<hyperlink>"],
+                    correctAnwer: 1,
+            },
         ]
     },
     {
         catagory: "CSS",
         questions : [
             {
+                question:"What does CSS stand for?",
+                options:["Computer Style Sheets"," Cascading Style Sheets",
+                    "Creative Style Sheets","Colorful Style Sheets"],
+                    correctAnwer: 1,
+            },
+            {
+                question:"Which property is used to change the background color?",
+                options:["color>","bgcolor",
+                    "background-color","background"],
+                    correctAnwer: 2,
+            },
+            {
+                question:"How do you select an element with id 'header' in CSS?",
+                options:[".header","#header",
+                    "header","*header"],
+                    correctAnwer: 1,
+            },
+              {
                 question:"What does CSS stand for?",
                 options:["Computer Style Sheets"," Cascading Style Sheets",
                     "Creative Style Sheets","Colorful Style Sheets"],
@@ -66,12 +102,48 @@ const  questions = [
                     "**","*##"],
                     correctAnwer: 1,
             },
+                 {
+                question:"Which company developed JavaScript?",
+                options:["Microsoft","Netscape",
+                    "Google","IBM"],
+                    correctAnwer: 1,
+            },
+            {
+                question:"How do you write 'Hello World' in an alert box?",
+                options:["alertBox('Hello World');","msg('Hello World');",
+                    "alert('Hello World');","msgBox('Hello World');"],
+                    correctAnwer: 2,
+            },
+            {
+                question:"Which symbol is used for comments in JavaScript?",
+                options:["<!-- -->","//",
+                    "**","*##"],
+                    correctAnwer: 1,
+            },
         ]
     },
        {
         catagory: "React.js",
         questions : [
             {
+                question:"What is React.js?",
+                options:["A back-end framework","A front-end library",
+                    "A database","A programming language"],
+                    correctAnwer: 1,
+            },
+            {
+                question:"Which company developed React?",
+                options:["Google","Microsoft",
+                    "Facebook (Meta)","Twitter"],
+                    correctAnwer: 2,
+            },
+            {
+                question:"What is JSX in React?",
+                options:["A JavaScript extension for XML-like syntax","A database query language",
+                    "A CSS framework","*A state management tool"],
+                    correctAnwer: 0,
+            },
+               {
                 question:"What is React.js?",
                 options:["A back-end framework","A front-end library",
                     "A database","A programming language"],
